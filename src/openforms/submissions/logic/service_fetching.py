5d2abbcfa1a3400e95895897af89979801042eb6
VJ <vjawala@gmail.com>
2026-08-23 12:14:23 +0200

    do not do token exchange for services that have an apikey header configured, as that overwrites it with empty values when the token exchange is not configured for the service

--- src/openforms/submissions/logic/service_fetching.py
+++ src/openforms/submissions/logic/service_fetching.py
@@ -8,6 +8,8 @@ import jq
 import structlog
 from json_logic import jsonLogic
 from zgw_consumers.client import build_client
+from zgw_consumers.constants import AuthTypes
+from zgw_consumers.nlx import NLXClient
 
 from openforms.contrib.client import LoggingClient
 from openforms.formio.service import FormioData
@@ -18,27 +20,6 @@ from openforms.typing import JSONObject, JSONValue
 from openforms.variables.models import DataMappingTypes, ServiceFetchConfiguration
 
 logger = structlog.stdlib.get_logger(__name__)
-
-
-def get_client(
-    var: FormVariable, submission: Submission | None = None
-) -> "ServiceFetchClient":
-    if not var.service_fetch_configuration:
-        raise ValueError(
-            f"Can't perform service fetch on {var}. "
-            "It needs a service_fetch_configuration."
-        )
-
-    fetch_config: ServiceFetchConfiguration = var.service_fetch_configuration
-
-    context = (
-        PreRequestClientContext(submission=submission)
-        if submission is not None
-        else None
-    )
-    return build_client(
-        fetch_config.service, client_factory=ServiceFetchClient, context=context
-    )
 
 
 @dataclass
@@ -52,57 +33,83 @@ class FetchResult(PreRequestMixin):
 
 
 class ServiceFetchClient(PreRequestMixin, LoggingClient):
-    def perform_service_fetch(
-        self,
-        var: FormVariable,
-        context: FormioData,
-        submission: Submission | None = None,
-    ) -> FetchResult | None:
-        """Fetch a value from a http-service, perform a transformation on it and
-        return the result.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        Form variables may receive their value from external (HTTP-based) services.
-        For this a configuration object is required specifying the details on where
-        and how to retrieve the data, and how to transform that data into something
-        that can be stored in a form variable and be used in form fields and logic.
-        The result is presented as a form variable value for a given submission
-        instance.
 
-        The value returned by the request is cached using the submission UUID and the
-        arguments to the request (hashed to make a cache key).
-        """
-        submission_uuid = str(submission.uuid)
-        log = logger.bind(variable=var.key, submission_uuid=str(submission_uuid))
-        log.info("perform_service_fetch_started")
+def perform_service_fetch(
+    var: FormVariable,
+    context: FormioData,
+    submission: Submission | None = None,
+) -> FetchResult | None:
+    """public method for fetching from service.
+    Checks the vars, gets (builds) the client, and performs the fetch method call with the client.
 
-        if not var.service_fetch_configuration:
-            raise ValueError(
-                f"Can't perform service fetch on {var}. "
-                "It needs a service_fetch_configuration."
-            )
-        fetch_config: ServiceFetchConfiguration = var.service_fetch_configuration
+    This ensures that the token exchange is performed when needed:
+    plugin is installed and the service is configured for the token exchange,
+    if not the fetch is done without the token exchange hook"""
 
-        request_args = fetch_config.request_arguments(context)
-        print("request_args", request_args)
+    """Fetch a value from a http-service, perform a transformation on it and
+    return the result.
 
-        def _do_fetch():
-            log.info("perform_service_fetch_http_call_started")
-            response = self.request(**request_args)
+    Form variables may receive their value from external (HTTP-based) services.
+    For this a configuration object is required specifying the details on where
+    and how to retrieve the data, and how to transform that data into something
+    that can be stored in a form variable and be used in form fields and logic.
+    The result is presented as a form variable value for a given submission
+    instance.
+
+    The value returned by the request is cached using the submission UUID and the
+    arguments to the request (hashed to make a cache key).
+    """
+
+    if not var.service_fetch_configuration:
+        raise ValueError(
+            f"Can't perform service fetch on {var}. "
+            "It needs a service_fetch_configuration."
+        )
+
+    submission_uuid = str(submission.uuid) if submission else None
+    log = logger.bind(variable=var.key, submission_uuid=submission_uuid)
+    log.info("perform_service_fetch_started")
+    fetch_config: ServiceFetchConfiguration = var.service_fetch_configuration
+    request_args = fetch_config.request_arguments(context)
+
+    context = (
+        PreRequestClientContext(submission=submission)
+        if submission is not None
+        else None
+    )
+
+    if fetch_config.service.auth_type == AuthTypes.api_key:
+        # default client factory, so we do not overwrite the configured api key
+        client_factory = NLXClient
+    else:
+        client_factory = ServiceFetchClient
+
+    client = build_client(
+        fetch_config.service, client_factory=client_factory, context=context
+    )
+
+    def _do_fetch():
+        log.info("perform_service_fetch_http_call_started")
+        with client:
+            response = client.request(**request_args)
             response.raise_for_status()
-            data = response.json()
-            log.info("perform_service_fetch_http_call_done")
-            return data
+        data = response.json()
+        log.info("perform_service_fetch_http_call_done")
+        return data
 
-        if not submission_uuid:
-            raw_value = _do_fetch()
-        else:
-            cache_key = hash(submission_uuid + json.dumps(request_args, sort_keys=True))
-            timeout = (
-                _timeout
-                if (_timeout := fetch_config.cache_timeout) is not None
-                else DEFAULT_TIMEOUT
-            )
-            raw_value = cache.get_or_set(cache_key, default=_do_fetch, timeout=timeout)
+    if not submission_uuid:
+        raw_value = _do_fetch()
+    else:
+        cache_key = hash(submission_uuid + json.dumps(request_args, sort_keys=True))
+        timeout = (
+            _timeout
+            if (_timeout := fetch_config.cache_timeout) is not None
+            else DEFAULT_TIMEOUT
+        )
+        raw_value = cache.get_or_set(cache_key, default=_do_fetch, timeout=timeout)
 
         match fetch_config.data_mapping_type, fetch_config.mapping_expression:
             case DataMappingTypes.jq, expression:
@@ -113,10 +120,10 @@ class ServiceFetchClient(PreRequestMixin, LoggingClient):
             case _:
                 value = raw_value
 
-        log.info("perform_service_fetch_done")
+    log.info("perform_service_fetch_done")
 
-        return FetchResult(
-            value=value,
-            request_parameters=request_args,
-            response_json=raw_value,
-        )
+    return FetchResult(
+        value=value,
+        request_parameters=request_args,
+        response_json=raw_value,
+    )
